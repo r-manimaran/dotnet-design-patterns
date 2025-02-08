ddba5d358789904af8083fce2409dd019d354e19
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-02-07 19:06:33 -0500

    Added IEntityTypeConfigurations for Category and product

---
+++ eCommerceApp/eCommerceApp.Infrastructure/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using eCommerceApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Infrastructure.Data.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasIndex(c => c.Name).IsUnique();
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Name)
+               .IsRequired()
+               .HasMaxLength(100);
+
+    }
+}
---
+++ eCommerceApp/eCommerceApp.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using eCommerceApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Infrastructure.Data.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+       builder.HasKey(p=>p.Id);
+
+        builder.Property(p => p.Name)
+             .IsRequired()
+             .HasMaxLength(100);
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(255);
+
+        builder.Property(p => p.Price)
+              .IsRequired()
+              .HasPrecision(18, 2);
+
+        builder.Property(p => p.Quantity)
+            .IsRequired();
+
+        builder.HasIndex(p => p.Name)
+            .IsUnique();
+    }
+}
