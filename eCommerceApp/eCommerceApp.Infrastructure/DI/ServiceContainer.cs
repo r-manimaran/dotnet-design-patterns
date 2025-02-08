ccb842a9f867003e7c57feb792c9d9b8245b5133
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-02-07 19:04:58 -0500

    Added ExceptionHandlingMiddleware.cs in the infrastrucure

--- eCommerceApp/eCommerceApp.Infrastructure/DI/ServiceContainer.cs
+++ eCommerceApp/eCommerceApp.Infrastructure/DI/ServiceContainer.cs
@@ -5,6 +5,9 @@ using eCommerceApp.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using EntityFramework.Exceptions.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using eCommerceApp.Infrastructure.Middlewares;
 
 namespace eCommerceApp.Infrastructure.DI;
 
@@ -19,13 +22,19 @@ public static class ServiceContainer
             {
                 sqlOptions.EnableRetryOnFailure();
                 sqlOptions.MigrationsAssembly(typeof(ServiceContainer).Assembly.FullName);
-            }),
+            }).UseExceptionProcessor(),
             ServiceLifetime.Scoped);
 
         services.AddScoped<IGenericRepository<Product>, GenericRepository<Product>>();
         services.AddScoped<IGenericRepository<Category>, GenericRepository<Category>>();
         
         return services;
+    }
+
+    public static IApplicationBuilder UserInfrastructureService(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+        return app;
     }
 
     
---
+++ eCommerceApp/eCommerceApp.Infrastructure/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace eCommerceApp.Infrastructure.Middlewares;
+
+public class ExceptionHandlingMiddleware(RequestDelegate _next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (DbException ex)
+        {
+            if (ex.InnerException is SqlException innerException)
+            {
+                switch (innerException.Number)
+                {
+                    case 2627: // unique constraint violation
+                        context.Response.StatusCode = StatusCodes.Status409Conflict;
+                        await context.Response.WriteAsync("Unique constraint violation");
+                        break;
+                    
+                    case 515: //cannot insert null
+                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                        await context.Response.WriteAsync("Cannot insert null");
+                        break;
+
+                    case 547: //Foreign key constraint violation
+                        context.Response.StatusCode= StatusCodes.Status409Conflict;
+                        await context.Response.WriteAsync("Foreign key constraint violation");
+                        break;
+
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await context.Response.WriteAsync("An error occured while saving the entity changes.");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode= StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsync("An error occured:"+ex.Message);
+        }
+    }
+}
